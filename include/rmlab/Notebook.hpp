0e5932379ec39f623f4b1322922b5a19da5d48d2
Axel Huebl <axel.huebl@plasma.ninja>
2017-12-28 21:30:32 +0100

    Add GPLv3+ Headers

--- include/rmlab/Notebook.hpp
+++ include/rmlab/Notebook.hpp
@@ -1,3 +1,22 @@
+/* Copyright 2017 Axel Huebl
+ *
+ * This file is part of lines-are-beautiful.
+ *
+ * lines-are-beautiful is free software: you can edistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * lines-are-beautiful is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with lines-are-beautiful.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #pragma once
 
 #include <cstdint>  // int8_t - int64_t
