80f301f27732c2c9a5d30f81a0385218e803ae5d
Axel Huebl <axel.huebl@plasma.ninja>
2018-01-08 20:56:21 +0100

    CMake Config: No Dependencies
    There are no PUBLIC dependencies in this project, so the
    Config.cmake file is quite simple

---
+++ RmlabConfig.cmake
@@ -0,0 +1,8 @@
+# only add PUBLIC dependencies as well
+#   https://cmake.org/cmake/help/latest/manual/cmake-packages.7.html#creating-a-package-configuration-file
+#include(CMakeFindDependencyMacro)
+#find_dependency(SomeLib 1.2.3)
+
+include("${CMAKE_CURRENT_LIST_DIR/RmlabTargets.cmake")
+
+# if necessary, add COMPONENTS here
